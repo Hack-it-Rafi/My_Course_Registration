1639e8576af203de94623483164e38a8001cf392
Hack-it-Rafi <zozorafi2016@gmail.com>
2023-09-15 16:19:29 +0600

    Api Built in Courses.json File

---
+++ my-project/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='flex justify-center my-5'>
+            <h1 className='text-3xl font-bold'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
